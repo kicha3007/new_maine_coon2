9b485a2fa802e4bfa785077dd9a5fca725704a25
Sergey K <kicha3007@gmail.com>
2017-07-28 07:35:37 +0500

    add information

--- markup/static/js/main.js
+++ markup/static/js/main.js
@@ -1,4 +1,4 @@
-$(document).ready(function() {
+/*$(document).ready(function() {
     var nav = $('.nav__list');
     $(document).on('click','.nav__trigger',function(){
         $('.nav__trigger').toggleClass('active');
@@ -15,7 +15,29 @@ $(document).ready(function() {
             nav.removeAttribute("style");
         }
     })
+});*/
+
+$(document).ready(function() {
+    var trigger = $('.nav__trigger');
+    var nav = $('.nav');
+
+    $(trigger).on('click',function(e){
+        $('.nav__trigger').toggleClass('active');
+        e.preventDefault();
+        nav.slideToggle();
+    });
+
+    $(window).resize(function(){
+        var w = $(window).width();
+        if(w > 750 && nav.is(':hidden')) {
+            nav.removeAttr("style");
+        }
+    })
 });
+
+
+
+
 
 $('#slider1').owlCarousel({
     loop:true,
